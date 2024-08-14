124f8896319054f5115204fffd2767eeffb1db1c
pianist1611 <121388008+pianist1611@users.noreply.github.com>
2024-08-13 19:55:12 -0500

    toDo list

---
+++ src/todolist/index.js
@@ -0,0 +1,31 @@
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const mainDiv = document.querySelector(".mainDiv");
+const tasks = [];
+
+button.addEventListener("click", () => {
+  mainDiv.textContent = "";
+  addTask(input.value);
+  displayTasks(tasks);
+  input.value = "";
+});
+const addTask = (task) => {
+  tasks.push(task);
+};
+const displayTasks = (tasks) => {
+  mainDiv.textContent = "";
+  tasks.forEach((element, index) => {
+    const task = document.createElement("div");
+    task.classList.add("task");
+    const removebutton = document.createElement("button");
+    removebutton.textContent = "X";
+    task.textContent = `${element}`;
+    removebutton.addEventListener("click", () => removeTask(index));
+    task.append(removebutton);
+    mainDiv.append(task);
+  });
+};
+const removeTask = (taskid) => {
+  tasks.splice(taskid, 1);
+  displayTasks(tasks);
+};
